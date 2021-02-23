b41aa3d4852f14a9652448be09631190988224a4
samarthtehri <samarth@psu.edu>
2021-02-24 03:45:41 +0400

    backup

---
+++ app/src/main/java/com/example/flix/DetailActivity.java
@@ -0,0 +1,38 @@
+package com.example.flix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.example.flix.models.Movie;
+
+import org.parceler.Parcels;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private static final String YOUTUBE_API_KEY = "AIzaSyD0N1auhopFc7gg1S2FHiiOIppGcgYW4fk";
+
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar ratingBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        tvTitle = findViewById(R.id.tvTitle);
+        tvOverview = findViewById(R.id.tvOverview);
+        ratingBar = findViewById(R.id.ratingBar);
+
+
+        String title = getIntent().getStringExtra("title");
+        Movie movie = Parcels.unwrap(getIntent().getParcelableExtra("movie"));
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+        ratingBar.setRating((float) movie.getRating());
+    }
+}
